11c9e8dd00794dd7795a94e02c704ecba3e574aa
Daksh Tandel <167323670+dakshtandel-9@users.noreply.github.com>
2025-07-08 22:45:18 +0530

    Fix ESLint warnings, disable no-img-element rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,15 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+
+  // Override specific rules here
+  {
+    rules: {
+      "@next/next/no-img-element": "off", // 👈 disables <img> warning
+    },
+  },
+];
 
 export default eslintConfig;
